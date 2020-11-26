1407e9af232491adf8f2405fb1ccd188459401ba
Adabs Urdum <cyrill@adabs.ch>
2020-11-26 13:26:56 +0100

    update content and ga tags

--- wp-content/themes/wp-setup/header.php
+++ wp-content/themes/wp-setup/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?> class="">
 <head>
 
-<!-- adabs.ch -->
+<!-- adabs.ch, not religious -->
 
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,21 +10,21 @@
 
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-76879621-3"></script> -->
-<!-- <script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-76879621-3"></script>
+<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-76879621-3', { 'anonymize_ip': true });
+  gtag('config', 'G-7V0DG6MZWE', { 'anonymize_ip': true });
 </script>
 <script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-2092620-46', 'auto');
-</script> -->
+ga('create', 'G-7V0DG6MZWE', 'auto');
+</script>
 
 
 <?php wp_head(); ?>
